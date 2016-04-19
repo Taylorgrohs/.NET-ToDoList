9c1daf7222849b14d8600b16f50633c337f9c06f
Taylor Grohs <taylorgrohs@gmail.com>
2016-04-19 10:15:07 -0700

    add category one to many relationship to to do list

--- ToDoList/src/ToDoList/Controllers/ItemsController.cs
+++ ToDoList/src/ToDoList/Controllers/ItemsController.cs
@@ -5,18 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using ToDoList.Models;
 using Microsoft.Data.Entity;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.AspNet.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
         private ToDoListContext db = new ToDoListContext();
-        // GET: /<controller>/
+
         public IActionResult Index()
         {
-            return View(db.Items.ToList());
+            return View(db.Items.Include(x => x.Category).ToList());
         }
         public IActionResult Details(int id)
         {
@@ -26,6 +25,7 @@ namespace ToDoList.Controllers
 
         public IActionResult Create()
         {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
             return View();
         }
 
@@ -40,6 +40,7 @@ namespace ToDoList.Controllers
         public IActionResult Edit(int id)
         {
             var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
             return View(thisItem);
         }
 
--- ToDoList/src/ToDoList/Models/ToDoListContext.cs
+++ ToDoList/src/ToDoList/Models/ToDoListContext.cs
@@ -3,11 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ToDoList.Models
 {
     public class ToDoListContext : DbContext
     {
+        public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Item> Items { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
